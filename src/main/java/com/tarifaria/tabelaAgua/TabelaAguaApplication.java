fdc71a18894f9989b60edba66b514fb6745c5e60
davidbjjj <pondavid103@gmail.com>
2026-02-05 08:51:15 -0300

    commit dos pacotes iniciais

---
+++ src/main/java/com/tarifaria/tabelaAgua/TabelaAguaApplication.java
@@ -0,0 +1,13 @@
+package com.tarifaria.tabelaAgua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelaAguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaAguaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarifaria/tabelaAgua/TabelaAguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarifaria.tabelaAgua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaAguaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
